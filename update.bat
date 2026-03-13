3a3669c041b826530cc29289a8baf30ae372457a
Dade Sugiono <emailkamu@gmail.com>
2026-03-13 10:57:23 +0700

    Update otomatis 13/03/2026 10.57.23,74

--- update.bat
+++ update.bat
@@ -1,21 +1,21 @@
 @echo off
 cd /d "C:\Users\dade\Documents\LMS pembelajaran"
-echo === CEK STATUS ===
+echo === CEK STATUS AWAL ===
 git status
 echo.
+
 echo === ADD SEMUA FILE ===
 git add .
 echo.
-echo === CEK STATUS LAGI (HARUSNYA HIJAU) ===
-git status
-echo.
+
 echo === COMMIT ===
-set /p pesan="Masukkan pesan update: "
-git commit -m "%pesan%"
+git commit -m "Update otomatis %date% %time%"
 echo.
+
 echo === PUSH KE GITHUB ===
 git push -f origin main
 echo.
+
 echo === SELESAI ===
 echo Cek di: https://github.com/dade92-gr/tembokbelajar
 pause
